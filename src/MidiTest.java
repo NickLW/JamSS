0d6418f097648c109ba2681964390281a24dd81e
Nick W <N.Walker3@uni.brighton.ac.uk>
2015-04-20 20:41:04 +0100

    Working options. (Can't change tempo mid jam)

--- src/MidiTest.java
+++ src/MidiTest.java
@@ -1,4 +1,4 @@
-       /*
+/*
 DEVELOPING GAME IN JAVA 
 
 Caracteristiques
@@ -10,7 +10,7 @@ Pages : 972
 Isbn : 1-59273-005-1 
 Reliure : Paperback 
 Disponibilite : Disponible a la librairie 
-*/
+ */
 
 
 import java.io.BufferedInputStream;
@@ -34,69 +34,75 @@ import javax.sound.midi.Track;
  */
 public class MidiTest implements MetaEventListener {
 
-  // The drum track in the example Midi file
-  private static final int DRUM_TRACK = 1;
-  public float tempo = 72;
-  
+	// The drum track in the example Midi file
+	private static final int DRUM_TRACK = 1;
+	public float tempo = 72;
+	String beat = "";
 
-  /*public static void main(String[] args) {
+
+	/*public static void main(String[] args) {
     new MidiTest().run();
   }*/
 
-  public MidiPlayer player;
-  
-  public void setBPM(int beatsPerMinute){
-	  tempo = beatsPerMinute;
-	  
-  }
-   
+	public MidiPlayer player;
 
-  public void run() {
+	public void setBPM(int beatsPerMinute){
+		tempo = beatsPerMinute;
 
-    player = new MidiPlayer();
-    
-    // load a sequence
-    Sequence sequence = player.getSequence("src/samples/072ChorusRide16thsF6b.mid");
-    sequence.createTrack();
-    
-
-    // turn off the drums
-    System.out.println("Playing (without drums)...");
-    Sequencer sequencer = player.getSequencer();
-    sequencer.setTempoInBPM(tempo);
-    
-    try {
-		sequencer.setSequence(sequence);
-	} catch (InvalidMidiDataException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
 	}
-    try {
-		sequencer.open();
-	} catch (MidiUnavailableException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-    sequencer.setTempoInBPM(tempo);
-    
-    //sequencer.setTrackMute(DRUM_TRACK, false);
-    sequencer.addMetaEventListener(this);
-    
- // play the sequence
-    sequencer.setTempoInBPM(tempo);
-    player.setBPMs(tempo);
-    player.play(sequence, false);
-  }
 
-  /**
-   * This method is called by the sound system when a meta event occurs. In
-   * this case, when the end-of-track meta event is received, the drum track
-   * is turned on.
-   */
-  public void meta(MetaMessage event) {
-    if (event.getType() == MidiPlayer.END_OF_TRACK_MESSAGE) {
-      Sequencer sequencer = player.getSequencer();
-      /*if (sequencer.getTrackMute(DRUM_TRACK)) {
+	public void setBeat(String track){
+		beat = track;
+
+	}
+
+
+	public void run() {
+
+		player = new MidiPlayer();
+
+		// load a sequence
+		Sequence sequence = player.getSequence("src/samples/072ChorusRide16thsF6b.mid");
+		sequence.createTrack();
+
+
+		// turn off the drums
+		System.out.println("Playing (without drums)...");
+		Sequencer sequencer = player.getSequencer();
+		sequencer.setTempoInBPM(tempo);
+
+		try {
+			sequencer.setSequence(sequence);
+		} catch (InvalidMidiDataException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			sequencer.open();
+		} catch (MidiUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sequencer.setTempoInBPM(tempo);
+
+		//sequencer.setTrackMute(DRUM_TRACK, false);
+		sequencer.addMetaEventListener(this);
+
+		// play the sequence
+		sequencer.setTempoInBPM(tempo);
+		player.setBPMs(tempo);
+		player.play(sequence, false);
+	}
+
+	/**
+	 * This method is called by the sound system when a meta event occurs. In
+	 * this case, when the end-of-track meta event is received, the drum track
+	 * is turned on.
+	 */
+	public void meta(MetaMessage event) {
+		if (event.getType() == MidiPlayer.END_OF_TRACK_MESSAGE) {
+			Sequencer sequencer = player.getSequencer();
+			/*if (sequencer.getTrackMute(DRUM_TRACK)) {
         // turn on the drum track
         System.out.println("Turning on drums...");
         sequencer.setTrackMute(DRUM_TRACK, false);
@@ -106,168 +112,169 @@ public class MidiTest implements MetaEventListener {
         //player.close();
         //System.exit(0);
       }*/
-    }
-  }
+		}
+	}
 
 }
 
 class MidiPlayer implements MetaEventListener {
 
-  // Midi meta event
-  public static final int END_OF_TRACK_MESSAGE = 47;
+	// Midi meta event
+	public static final int END_OF_TRACK_MESSAGE = 47;
 
-  private Sequencer sequencer;
-  
-  public float tempo;
-  
-  public void setBPMs(float beatsPerMinute){
-	  tempo = beatsPerMinute;
-  }
+	private Sequencer sequencer;
 
-  private boolean loop;
+	public float tempo;
 
-  public boolean paused;
+	public void setBPMs(float beatsPerMinute){
+		tempo = beatsPerMinute;
+	}
 
-  /**
-   * Creates a new MidiPlayer object.
-   */
-  public MidiPlayer() {
-    try {
-      sequencer = MidiSystem.getSequencer();
-      sequencer.open();
-      sequencer.addMetaEventListener(this);
-      sequencer.setTempoInBPM(tempo);
-    } catch (MidiUnavailableException ex) {
-      sequencer = null;
-    }
-  }
+	private boolean loop;
 
-  /**
-   * Loads a sequence from the file system. Returns null if an error occurs.
-   */
-  public Sequence getSequence(String filename) {
-    try {
-      return getSequence(new FileInputStream(filename));
-    } catch (IOException ex) {
-      ex.printStackTrace();
-      return null;
-    }
-  }
+	public boolean paused;
 
-  /**
-   * Loads a sequence from an input stream. Returns null if an error occurs.
-   */
-  public Sequence getSequence(InputStream is) {
-    try {
-      if (!is.markSupported()) {
-        is = new BufferedInputStream(is);
-      }
-      Sequence s = MidiSystem.getSequence(is);
-      is.close();
-      return s;
-    } catch (InvalidMidiDataException ex) {
-      ex.printStackTrace();
-      return null;
-    } catch (IOException ex) {
-      ex.printStackTrace();
-      return null;
-    }
-  }
+	/**
+	 * Creates a new MidiPlayer object.
+	 */
+	public MidiPlayer() {
+		try {
+			sequencer = MidiSystem.getSequencer();
+			sequencer.open();
+			sequencer.addMetaEventListener(this);
+			sequencer.setTempoInBPM(tempo);
+		} catch (MidiUnavailableException ex) {
+			sequencer = null;
+		}
+	}
 
-  /**
-   * Plays a sequence, optionally looping. This method returns immediately.
-   * The sequence is not played if it is invalid.
-   */
-  public void play(Sequence sequence, boolean loop) {
-    if (sequencer != null && sequence != null && sequencer.isOpen()) {
-      try {
-        sequencer.setSequence(sequence);
-        sequencer.open();
-        sequencer.setTempoInBPM(tempo);
-        /*if(loop) {
+	/**
+	 * Loads a sequence from the file system. Returns null if an error occurs.
+	 */
+	public Sequence getSequence(String filename) {
+		try {
+			return getSequence(new FileInputStream(filename));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Loads a sequence from an input stream. Returns null if an error occurs.
+	 */
+	public Sequence getSequence(InputStream is) {
+		try {
+			if (!is.markSupported()) {
+				is = new BufferedInputStream(is);
+			}
+			Sequence s = MidiSystem.getSequence(is);
+			is.close();
+			return s;
+		} catch (InvalidMidiDataException ex) {
+			ex.printStackTrace();
+			return null;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Plays a sequence, optionally looping. This method returns immediately.
+	 * The sequence is not played if it is invalid.
+	 */
+	public void play(Sequence sequence, boolean loop) {
+		if (sequencer != null && sequence != null && sequencer.isOpen()) {
+			try {
+				sequencer.setSequence(sequence);
+				sequencer.open();
+				sequencer.setTempoInBPM(tempo);
+				/*if(loop) {
             sequencer.setLoopStartPoint(0);
             sequencer.setLoopEndPoint(-1);
             sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
             sequencer.setTempoInBPM(tempo);
         }*/
-        //sequencer.setTempoFactor(1.0F);
-        sequencer.setTempoInBPM(tempo);
-        sequencer.start();
-        //sequencer.setTempoFactor(1.0F);
-        sequencer.setTempoInBPM(tempo);
-        
-        this.loop = loop;
-      } catch (InvalidMidiDataException ex) {
-        ex.printStackTrace();
-      } catch (MidiUnavailableException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+				//sequencer.setTempoFactor(1.0F);
+				sequencer.setTempoInBPM(tempo);
+				sequencer.start();
+				//sequencer.setTempoFactor(1.0F);
+				sequencer.setTempoInBPM(tempo);
+
+				this.loop = loop;
+			} catch (InvalidMidiDataException ex) {
+				ex.printStackTrace();
+			} catch (MidiUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
-    }
-  }
 
-  /**
-   * This method is called by the sound system when a meta event occurs. In
-   * this case, when the end-of-track meta event is received, the sequence is
-   * restarted if looping is on.
-   */
-  public void meta(MetaMessage event) {
-    if (event.getType() == END_OF_TRACK_MESSAGE) {
-      if (sequencer != null && sequencer.isOpen() && loop) {
-    	sequencer.setMicrosecondPosition(0);
-    	sequencer.setTempoInBPM(tempo);
-        sequencer.start();
-        sequencer.setTempoInBPM(tempo);
-      }
-    }
-  }
+	/**
+	 * This method is called by the sound system when a meta event occurs. In
+	 * this case, when the end-of-track meta event is received, the sequence is
+	 * restarted if looping is on.
+	 */
+	public void meta(MetaMessage event) {
+		if (event.getType() == END_OF_TRACK_MESSAGE) {
+			if (sequencer != null && sequencer.isOpen() && loop) {
+				sequencer.setMicrosecondPosition(0);
+				sequencer.setTempoInBPM(tempo);
+				sequencer.start();
+				sequencer.setTempoInBPM(tempo);
+			}
+		}
+	}
 
-  /**
-   * Stops the sequencer and resets its position to 0.
-   */
-  public void stop() {
-    if (sequencer != null && sequencer.isOpen()) {
-      sequencer.stop();
-      sequencer.setMicrosecondPosition(0);
-      sequencer.setTempoInBPM(tempo);
-    }
-  }
+	/**
+	 * Stops the sequencer and resets its position to 0.
+	 */
+	public void stop() {
+		if (sequencer != null && sequencer.isOpen()) {
+			sequencer.stop();
+			sequencer.setMicrosecondPosition(0);
+			sequencer.setTempoInBPM(tempo);
+		}
+	}
 
-  /**
-   * Closes the sequencer.
-   */
-  public void close() {
-    if (sequencer != null && sequencer.isOpen()) {
-      sequencer.close();
-    }
-  }
+	/**
+	 * Closes the sequencer.
+	 */
+	public void close() {
+		if (sequencer != null && sequencer.isOpen()) {
+			sequencer.close();
+		}
+	}
 
-  /**
-   * Gets the sequencer.
-   */
-  public Sequencer getSequencer() {
-    return sequencer;
-  }
+	/**
+	 * Gets the sequencer.
+	 */
+	public Sequencer getSequencer() {
+		return sequencer;
+	}
 
-  /**
-   * Sets the paused state. Music may not immediately pause.
-   */
-  public void setPaused(boolean paused) {
-    if (this.paused != paused && sequencer != null && sequencer.isOpen()) {
-      this.paused = paused;
-      if (paused) {
-        sequencer.stop();
-      } else {
-        sequencer.start();
-      }
-    }
-  }
+	/**
+	 * Sets the paused state. Music may not immediately pause.
+	 */
+	public void setPaused(boolean paused) {
+		if (this.paused != paused && sequencer != null && sequencer.isOpen()) {
+			this.paused = paused;
+			if (paused) {
+				sequencer.stop();
+			} else {
+				sequencer.start();
+			}
+		}
+	}
 
-  /**
-   * Returns the paused state.
-   */
-  public boolean isPaused() {
-    return paused;
-  }
+	/**
+	 * Returns the paused state.
+	 */
+	public boolean isPaused() {
+		return paused;
+	}
+
 
 }
--- src/Scale.java
+++ src/Scale.java
@@ -1,70 +1,200 @@
 
 public class Scale {
-	
-	private static final double[] FFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] FNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    
+
+	//MAJOR SCALES
+
+	private static final double[] FFREQUENCIES = { 174.61, 164.81, 146.83, 130.81, 116.54, 110.00, 98.00, 87.31};
+	private static final String[] FNAME        = { "F",    "E",    "D",    "C",    "A#",   "A",    "G",   "F"};
+
 	private static final double[] EFREQUENCIES = {164.81, 155.56, 138.59, 123.47, 110.00, 103.83, 92.50, 82.41};
-    private static final String[] ENAME        = {"E",    "D#",    "C#",   "B",    "A",    "G#",   "F#",  "E"};
-    
-    
+	private static final String[] ENAME        = {"E",    "D#",    "C#",   "B",    "A",    "G#",   "F#",  "E"};
 
-    private static final double[] DsFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] DsNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] DFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] DNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] CsFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] CsNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] CFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] CNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] BFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] BNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] AsFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] AsNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] AFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] ANAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] GsFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] GsNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] GFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] GNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
-    
-    private static final double[] FsFREQUENCIES = { 174.61, 164.81, 155.56, 146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
-    private static final String[] FsNAME        = { "F",    "E",    "D#",   "D",    "C#",   "C",    "B",    "A#",   "A",    "G#",   "G",   "F#",  "F",   "E",   "D#"};
+	private static final double[] DsFREQUENCIES = { 155.56, 146.83, 130.81, 116.54, 103.83, 98.00, 87.31, 77.78};
+	private static final String[] DsNAME        = { "D#",   "D",    "C",    "A#",   "G#",   "G",   "F",   "D#"};
 
-    
-    
-    
-    
-    //MINOR SCALES
-    
-    private static final double[] EFREQUENCIESm = {164.81, 146.83, 130.81, 123.47, 110.00, 98.00, 92.50, 82.41};
-    private static final String[] ENAMEm        = {"E",    "D",    "C",    "B",    "A",    "G",   "F#",   "E"};
-    
+	private static final double[] DFREQUENCIES = { 146.83, 138.59, 123.47, 110.00, 98.00, 92.50, 82.41, 73.42};
+	private static final String[] DNAME        = { "D",    "C#",   "B",    "A",    "G",   "F#",  "E",    "D"};
 
-    
-    public static String[] getScale(String root, String scale){
-    	if(scale == "Major"){
-    		return ENAME;		
-    	} else {
-    		return ENAMEm;
-    	}
-    }
-    
-    public static double[] getFreq(String root, String scale){
-    	if(scale == "Major"){
-    		return EFREQUENCIES;		
-    	} else {
-    		return EFREQUENCIESm;
-    	}		
-    }
-    
+	private static final double[] CsFREQUENCIES = { 138.59, 130.81, 116.54, 103.83, 92.50, 87.31, 77.78, 69.30};
+	private static final String[] CsNAME        = { "C#",   "C",    "A#",   "G#",   "F#",  "F",   "D#",  "C#"};
+
+	private static final double[] CFREQUENCIES = { 130.81, 123.47, 110.00, 98.00, 87.31, 82.41, 73.42, 65.41};
+	private static final String[] CNAME        = { "C",    "B",    "A",    "G",   "F",   "E",    "D",    "C"};
+
+	private static final double[] BFREQUENCIES = { 246.94, 233.08, 207.65, 185.00, 164.81, 155.56, 138.59, 123.47};
+	private static final String[] BNAME        = { "B",    "A#",   "G#",   "F#",   "E",    "D#",   "C#",   "B"};
+
+	private static final double[] AsFREQUENCIES = { 233.08, 220.00, 196.00, 174.61, 155.56, 146.83, 130.81, 116.54};
+	private static final String[] AsNAME        = { "A#",   "A",    "G",    "F",    "D#",   "D",    "C",    "A#"};
+
+	private static final double[] AFREQUENCIES = { 220.00, 207.65, 185.00, 164.81, 146.83, 138.59, 123.47, 110.00};
+	private static final String[] ANAME        = { "A",    "G#",   "F#",   "E",    "D",    "C#",   "B",    "A"};
+
+	private static final double[] GsFREQUENCIES = { 207.65, 196.00, 174.61, 155.56, 138.59, 130.81, 116.54, 103.83};
+	private static final String[] GsNAME        = { "G#",   "G",    "F",    "D#",   "C#",   "C",    "A#",   "G#"};
+
+	private static final double[] GFREQUENCIES = { 196.00, 185.00, 164.81, 146.83, 130.81, 123.47, 110.00, 98.00};
+	private static final String[] GNAME        = { "G",    "F#",   "E",    "D",    "C",    "B",    "A",    "G"};
+
+	private static final double[] FsFREQUENCIES = { 185.00, 174.61, 155.56, 138.59, 123.47, 116.54, 103.83, 92.50};
+	private static final String[] FsNAME        = { "F#",   "F",    "D#",   "C#",   "B",    "A#",   "G#",   "F#"};
+
+
+	//MINOR SCALES
+
+	private static final double[] FFREQUENCIESm = {174.61, 155.56, 138.59, 130.81, 116.54, 103.83, 98.00, 87.31};
+	private static final String[] FNAMEm        = {"F",    "D#",    "C#",    "C",    "A#",   "G#",   "G",   "F"};
+
+	private static final double[] EFREQUENCIESm = {164.81, 146.83, 130.81, 123.47, 110.00, 98.00, 92.50, 82.41};
+	private static final String[] ENAMEm        = {"E",    "D",    "C",    "B",    "A",    "G",   "F#",   "E"};
+
+	private static final double[] DsFREQUENCIESm = {155.56, 138.59, 123.47, 116.54, 103.83, 92.50, 87.31, 77.78};
+	private static final String[] DsNAMEm        = {"D#",   "C#",   "B",    "A#",   "G#",   "F#",  "F",   "D#"};
+
+	private static final double[] DFREQUENCIESm = {146.83, 130.81, 116.54, 110.00, 98.00, 87.31, 82.41, 73.42};
+	private static final String[] DNAMEm        = {"D",    "C",    "A#",    "A",    "G",   "F",   "E",  "D"};
+
+	private static final double[] CsFREQUENCIESm = {138.59, 123.47, 110.00, 103.83, 92.50, 82.41, 77.78, 69.30};
+	private static final String[] CsNAMEm        = {"C#",    "B",    "A",    "G#",   "F#",   "E", "D#",  "C#"};
+
+	private static final double[] CFREQUENCIESm = {130.81, 116.54, 103.83, 98.00, 87.31, 77.78, 73.42, 65.41};
+	private static final String[] CNAMEm        = {"C",    "A#",    "G#",    "G",   "F",   "D#",    "D",    "C"};
+
+	private static final double[] BFREQUENCIESm = {246.94, 220.00, 196.00, 185.00, 164.81, 146.83, 138.59, 123.47};
+	private static final String[] BNAMEm        = {"B",    "A",    "G",    "F#",   "E",    "D",    "C#",   "B"};
+
+	private static final double[] AsFREQUENCIESm = {233.08, 207.65, 185.00, 174.61, 155.56, 138.59, 130.81, 116.54};
+	private static final String[] AsNAMEm        = {"A#",   "G#",   "F#",   "F",    "D#",   "C#",   "C",   "A#"};
+
+	private static final double[] AFREQUENCIESm = {220.00, 196.00, 174.61, 164.81, 146.83, 130.81, 123.47, 110.00};
+	private static final String[] ANAMEm        = {"A",    "G",    "F",    "E",    "D",    "C",    "B",    "A"};
+
+	private static final double[] GsFREQUENCIESm = {207.65, 185.00, 164.81, 155.56, 138.59, 123.47, 116.54, 103.83};
+	private static final String[] GsNAMEm        = {"G#",   "F#",   "E",    "D#",   "C#",   "B",    "A#",   "G#"};
+
+	private static final double[] GFREQUENCIESm = {196.00, 174.61, 155.56, 146.83, 130.81, 116.54, 110.00, 98.00};
+	private static final String[] GNAMEm        = {"G",    "F",    "D#",   "D",    "C",    "A#",   "A",    "G"};
+
+	private static final double[] FsFREQUENCIESm = {185.00, 164.81, 146.83, 138.59, 123.47, 110.00, 103.83, 92.50};
+	private static final String[] FsNAMEm        = {"F#",   "E",    "D",    "C#",   "B",    "A",    "G#",   "F#"};
+
+
+	public static String[] getScale(String root, String scale){
+		String[] ret = ENAME;
+
+		if(scale == "Major"){
+			if (root == "E"){
+				ret = ENAME;	
+			} else if (root == "F"){
+				ret = FNAME;
+			} else if (root == "F#"){
+				ret = FNAME;
+			} else if (root == "G"){
+				ret = GNAME;
+			} else if (root == "G#"){
+				ret = GsNAME;
+			} else if (root == "A"){
+				ret = ANAME;
+			} else if (root == "A#"){
+				ret = AsNAME;
+			} else if (root == "B"){
+				ret = BNAME;
+			} else if (root == "C"){
+				ret = CNAME;
+			} else if (root == "C#"){
+				ret = CsNAME;
+			} else if (root == "D"){
+				ret = DNAME;
+			} else if (root == "D#"){
+				ret = DsNAME;
+			}
+		} else if (scale == "Minor"){
+			if (root == "E"){
+				ret = ENAMEm;	
+			} else if (root == "F"){
+				ret = FNAMEm;
+			} else if (root == "F#"){
+				ret = FNAMEm;
+			} else if (root == "G"){
+				ret = GNAMEm;
+			} else if (root == "G#"){
+				ret = GsNAMEm;
+			} else if (root == "A"){
+				ret = ANAMEm;
+			} else if (root == "A#"){
+				ret = AsNAMEm;
+			} else if (root == "B"){
+				ret = BNAMEm;
+			} else if (root == "C"){
+				ret = CNAMEm;
+			} else if (root == "C#"){
+				ret = CsNAMEm;
+			} else if (root == "D"){
+				ret = DNAMEm;
+			} else if (root == "D#"){
+				ret = DsNAMEm;
+			}
+		} 
+		return ret;
+	}
+
+	public static double[] getFreq(String root, String scale){
+		double[] ret = EFREQUENCIES;
+
+		if(scale == "Major"){
+			if (root == "E"){
+				ret = EFREQUENCIES;	
+			} else if (root == "F"){
+				ret = FFREQUENCIES;
+			} else if (root == "F#"){
+				ret = FsFREQUENCIES;
+			} else if (root == "G"){
+				ret = GFREQUENCIES;
+			} else if (root == "G#"){
+				ret = GsFREQUENCIES;
+			} else if (root == "A"){
+				ret = AFREQUENCIES;
+			} else if (root == "A#"){
+				ret = AsFREQUENCIES;
+			} else if (root == "B"){
+				ret = BFREQUENCIES;
+			} else if (root == "C"){
+				ret = CFREQUENCIES;
+			} else if (root == "C#"){
+				ret = CsFREQUENCIES;
+			} else if (root == "D"){
+				ret = DFREQUENCIES;
+			} else if (root == "D#"){
+				ret = DsFREQUENCIES;
+			}
+		} else if (scale == "Minor"){
+			if (root == "E"){
+				ret = EFREQUENCIESm;	
+			} else if (root == "F"){
+				ret = FFREQUENCIESm;
+			} else if (root == "F#"){
+				ret = FsFREQUENCIESm;
+			} else if (root == "G"){
+				ret = GFREQUENCIESm;
+			} else if (root == "G#"){
+				ret = GsFREQUENCIESm;
+			} else if (root == "A"){
+				ret = AFREQUENCIESm;
+			} else if (root == "A#"){
+				ret = AsFREQUENCIESm;
+			} else if (root == "B"){
+				ret = BFREQUENCIESm;
+			} else if (root == "C"){
+				ret = CFREQUENCIESm;
+			} else if (root == "C#"){
+				ret = CsFREQUENCIESm;
+			} else if (root == "D"){
+				ret = DFREQUENCIESm;
+			} else if (root == "D#"){
+				ret = DsFREQUENCIESm;
+			}
+		} 
+		return ret;		
+	}
+
 }
